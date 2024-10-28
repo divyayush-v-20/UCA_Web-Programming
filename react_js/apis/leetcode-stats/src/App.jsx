ddab25dc597296a31d5cae0773ebb8d8965e7d11
Divyayush Verma <divyayushv20@gmail.com>
2024-10-28 22:03:26 +0530

    comments

--- react_js/apis/leetcode-stats/src/App.jsx
+++ react_js/apis/leetcode-stats/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import { useRef } from 'react'
 import axios from "axios"
 import './App.css'
